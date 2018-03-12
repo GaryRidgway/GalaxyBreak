da12b09aea6d17619d15d14737427d9b9cf20cfa
GaryRidgway <gary-ridgway@uiowa.edu>
2018-03-12 01:52:00 -0500

    ading behavior for draggable nodes and parent nodes

--- js/jQuery/node-workspace.js
+++ js/jQuery/node-workspace.js
@@ -1,35 +1,63 @@
 $(document).ready( function() {
   var nws = $('#node-workspace');
   nws.click(function() {
-    addNode('body');
+    addNode(targets);
   });
 });
 
 // Track the Mouse.
+var targets = ['body'];
 var currentMousePos = { x: -1, y: -1 };
+var selectedNodes = [];
 $(document).mousemove(function(event) {
   currentMousePos.x = event.pageX;
   currentMousePos.y = event.pageY;
 });
 
 var NID = 0;
-function addNode(parent) {
-  // Make the node.
-  $(parent).append(''.concat(
-    "<div id = 'node-",NID,"' class = 'node'>\
-    </div>"
-  ));
-  let cNode = $('#node-'.concat(NID))
-  // Position it on the mouse.
-  cNode.css({
-    'left':''.concat(currentMousePos.x-5),
-    'top':''.concat(currentMousePos.y-5)
-  });
+function addNode(parents) {
+  if(parents.length == 1) {
+    // Make the node.
+    $(parents[0]).append(''.concat(
+      "<div id = 'node-",NID,"' class = 'node'>\
+      </div>"
+    ));
+    let cNode = $('#node-'.concat(NID))
+    // Position it on the mouse.
+    let nodeSize = $(':root').css('--node-size').replace(/[^-\d\.]/g, '');
+    cNode.css({
 
-  cNode.click(function(){
-    cNode.toggleClass('selected');
-  });
+      'left':''.concat(currentMousePos.x-nodeSize/2),
+      'top':''.concat(currentMousePos.y-nodeSize/2)
+    });
 
+    cNode.mousedown(function(){
+      selectNode(cNode);
+    });
 
+    // Allow it to be draggable.
+    dragElement(document.getElementById('node-'.concat(NID)));
+  }
+
+  // THIS SHOULD ALWAYS BE LAST.
   NID ++;
+}
+
+
+// This may have to be updated later to improve functionality,
+// specifically for multinode selections.
+function selectNode(target) {
+  // Specifically theis block might be a problem.
+  $('.node').each(function() {
+    $(this).removeClass('selected');
+  });
+  //
+  target.addClass('selected');
+  selectedNodes = [];
+  targets = [];
+  $('.selected').each(function() {
+    selectedNodes.push($(this).attr('id'));
+    targets.push('#' + $(this).attr('id'));
+  });
+
 }
