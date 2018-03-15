8f5be627d084d7254fe44ee5237cf5373c4c6e4f
GaryRidgway <gary-ridgway@uiowa.edu>
2018-03-14 23:53:58 -0500

    added text areas under nodes

--- js/jQuery/add-node.js
+++ js/jQuery/add-node.js
@@ -4,7 +4,11 @@ function addNode(parents) {
 
     // Make the node.
     $(parents[0]).append(''.concat(
-      "<div id = 'node-",NID,"' class = 'node'>\
+      "<div id='node-",NID,"' class='node'>\
+        <div class='textbox-positioner'id='textbox-positioner-",NID,"'>\
+          <div id='textbox-bg-",NID,"' class='textbox-bg'></div>\
+          <input type='text' class='node-input' id='node-input-",NID,"'>\
+        </div>\
       </div>"
     ));
 
@@ -39,8 +43,40 @@ function addNode(parents) {
     $(cNode).data( "draggable", new dragElement(document.getElementById('node-'.concat(NID))));
     // console.log($(cNode).data( "draggable"));
 
+    // Size the text boxes.
+    let cNodeTextBox = cNode.find('.textbox-positioner');
+    let cNodeTextBoxInputBG = cNodeTextBox.find('#textbox-bg-'.concat(NID));
+    let cNodeTextBoxInput = cNodeTextBox.find('#node-input-'.concat(NID));
+
+    cNodeTextBoxInputBG.css('height', cNodeTextBox.height());
+    cNodeTextBoxInput.css('top', '-' + (cNodeTextBox.height()/2));
+    cNodeTextBoxInput.on("change paste keyup", function() {
+      let cNodeTextWidth = $.fn.textWidth(cNodeTextBoxInput.val(), 'Roboto');
+      cNodeTextBox.css('width', cNodeTextWidth+10);
+      cNodeTextBoxInputBG.css('width', cNodeTextWidth+10);
+      cNodeTextBoxInput.css('width', cNodeTextWidth+10);
+      if(cNodeTextBoxInput.val() != '') {
+        cNodeTextBoxInputBG.css('background-color', 'rgba(43,43,43, 0.8)');
+      } else {
+        cNodeTextBoxInputBG.css('background-color', 'transparent');
+      }
+    });
+    cNodeTextBoxInput.keyup(function() {
+      let cNodeTextWidth = $.fn.textWidth(cNodeTextBoxInput.val(), 'Roboto');
+      cNodeTextBox.css('width', cNodeTextWidth+10);
+      cNodeTextBoxInputBG.css('width', cNodeTextWidth+10);
+      cNodeTextBoxInput.css('width', cNodeTextWidth+10);
+    });
+
   }
 
   // THIS SHOULD ALWAYS BE LAST.
   NID ++;
 }
+
+
+$.fn.textWidth = function(text, font) {
+  if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+  $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+  return $.fn.textWidth.fakeEl.width();
+};
